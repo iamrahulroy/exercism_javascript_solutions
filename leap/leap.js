6e7ca44a1409761be94457268e897d2016d600c2
Rahul Roy <techniciablog@gmail.com>
2016-04-26 17:42:03 +0530

    Use === instead of ==

--- leap/leap.js
+++ leap/leap.js
@@ -3,7 +3,7 @@ var Leap = function(year) {
 };
 
 Leap.prototype.isLeap = function(){
-  if(this.year % 100 == 0 && this.year % 400 !== 0) {
+  if(this.year % 100 === 0 && this.year % 400 !== 0) {
     return false;
   } else if(this.year % 4 === 0 || this.year % 400 === 0) {
     return true;
