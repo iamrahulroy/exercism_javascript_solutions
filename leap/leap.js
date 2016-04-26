21288c9f21b8c2340dd8ed57c14d2e22bc5be894
Rahul Roy <techniciablog@gmail.com>
2016-04-26 17:37:34 +0530

    Solve leap year problem

---
+++ leap/leap.js
@@ -0,0 +1,15 @@
+var Leap = function(year) {
+  this.year = year;
+};
+
+Leap.prototype.isLeap = function(){
+  if(this.year % 100 == 0 && this.year % 400 !== 0) {
+    return false;
+  } else if(this.year % 4 === 0 || this.year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+module.exports = Leap;
