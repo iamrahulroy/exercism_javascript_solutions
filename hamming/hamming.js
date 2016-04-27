84fa5eb4aca648f2920b44566dc538c1a57e1702
Rahul Roy <techniciablog@gmail.com>
2016-04-27 18:04:49 +0530

    Refactor hamming problem

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,14 +1,10 @@
 var Hamming = function() {};
 
 Hamming.prototype.compute = function(strand1, strand2){
-  if (strand1.length !== strand2.length) {
-    throw "DNA strands must be of equal length."
-  }
+  if (strand1.length !== strand2.length) { throw "DNA strands must be of equal length." }
   var score = 0;
   for(var i = 0; i < strand1.length; i++){
-    if(strand1[i] !== strand2[i]){
-      score = score + 1;
-    }
+    strand1[i] !== strand2[i] && score++;
   }
   return score;
 }
